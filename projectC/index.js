eb3f1b274763e1d0d431aa995ea31b108f5073e7
Zach Glazer <zach.glazer@legacymusicpartners.com>
2021-03-04 18:18:31 -0600

    rm space

--- projectC/index.js
+++ projectC/index.js
@@ -15,4 +15,4 @@ app.post('/api/message', (req, res) => {
 });
 
 const port = process.env.PORT || 4000;
- app.listen(port, () => console.log("Server is running..."));
+app.listen(port, () => console.log("Server is running..."));
