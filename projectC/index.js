ba893a6e27bd05c3873aa3145c51bd1b1baa9aef
Zach Glazer <zach.glazer@legacymusicpartners.com>
2021-03-04 18:16:17 -0600

    make some changes

--- projectC/index.js
+++ projectC/index.js
@@ -15,5 +15,5 @@ app.post('/api/message', (req, res) => {
 });
 
 const port = process.env.PORT || 4000;
-app.listen(port, () => console.log('Server is running...'));
+app.listen(port, () => console.log('Server is running on PORT ' + port + '...'));
 
